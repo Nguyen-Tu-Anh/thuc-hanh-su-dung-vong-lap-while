762d35eae7924c07035a212315253f9b0d854826
Nguyen-Tu-Anh <nguyentuanh.10t2@gmail.com>
2021-10-12 14:24:37 +0700

    first commit

---
+++ my.js
@@ -0,0 +1,8 @@
+let num = 0
+let sum = 0;
+while (num != -1) {
+    num = +prompt('moi nhap so');
+    console.log(num);
+    sum += num;
+}
+alert(sum);
---
+++ my2.js
@@ -0,0 +1,6 @@
+let i = 0;
+while  (i <=100) {
+    i += 1;
+    document.write("<hr width = " + i + "%>");
+    i++;
+}
